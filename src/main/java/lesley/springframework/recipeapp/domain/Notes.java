d4dc82ab24a172218ca153078cb8ab516badbb92
lesley okoduwa <leslieokoduwa@gmail.com>
2020-07-08 19:27:22 +0100

    initial commit

---
+++ src/main/java/lesley/springframework/recipeapp/domain/Notes.java
@@ -0,0 +1,40 @@
+package lesley.springframework.recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String notes) {
+        this.recipeNotes = notes;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
